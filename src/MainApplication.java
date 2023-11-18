ecbfca9498660924f32387d075db9fd72b21e604
Devin Diaz <diazdevin7@outlook.com>
2023-11-18 14:43:11 -0500

    successful test case with new password features and methods

--- src/MainApplication.java
+++ src/MainApplication.java
@@ -1,3 +1,5 @@
+//PATH TO PROJECT: /IdeaProjects/BankingApplication
+//For version control reasons
 import java.util.Scanner;
 
 public class MainApplication {
@@ -15,42 +17,14 @@ public class MainApplication {
         System.out.print("Change password (Y/N): ");
         String ans = sc.nextLine();
         if(ans.equalsIgnoreCase("y")) {
-            userPasswordChanger(user);
+            changingUserPassword(user);
         }
     }
 
-    private static void userPasswordChanger(CreatingUserAccount user) {
-        String currentUser = user.getUserDetails("username").toUpperCase();
-        Scanner sc = new Scanner(System.in);
-        String newPassword;
-        String oldPassword;
-
-        System.out.print("Please Enter Old Password: ");
-        oldPassword = sc.nextLine();
-
-        if(!user.passwordChecker(oldPassword)) {
-            do {
-                System.out.println("*NOT FOUND* PLEASE TRY AGAIN");
-                System.out.print("Please Enter Old Password: ");
-                oldPassword = sc.nextLine();
-            }while(!user.passwordChecker(oldPassword));
-        }
-        System.out.print("Enter New Password: ");
-        newPassword = sc.nextLine();
-
-        if(!user.passwordChangerConditions(newPassword)) {
-            do {
-                System.out.println("*INVALID SEQUENCE* MUST HAVE SPECIAL CHAR, UPPERCASE," +
-                        "NON FIRST AND LAST NAME, AND 8 OR MORE CHARACTERS!");
-                System.out.print("Enter New Password: ");
-                newPassword = sc.nextLine();
-            }while(!user.passwordChangerConditions(newPassword));
-        }
-
-        user.setUserDetails("password", newPassword);
-        System.out.println("UPDATE FOR : " + currentUser + "\n"+
-                "YOUR NEW CREDENTIALS: \n" +
-                "OLD PASSWORD: " + oldPassword + "\n" +
-                "NEW PASSWORD: " + user.getUserDetails("password"));
+    //new static void method to change users password
+    private static void changingUserPassword(CreatingUserAccount user) {
+        UserChangingDetails newUserPassword = new UserChangingDetails();
+        newUserPassword.userPasswordChangerFinal(user);
     }
+
 }
