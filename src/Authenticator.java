9dcbaafcb605183d274bf49e3dec24113311dcec
Devin Diaz <diazdevin7@outlook.com>
2023-11-19 21:05:07 -0500

    changes to passwordConditions interface method

--- src/Authenticator.java
+++ src/Authenticator.java
@@ -1,6 +1,8 @@
 public interface Authenticator {
 
     public boolean passwordChecker(String password);
-    public boolean passwordChangerConditions(String password);
+
+    //changed the name of this method since it will be used for passwords in general, not just password changes
+    public boolean passwordConditions(String password);
 
 }
